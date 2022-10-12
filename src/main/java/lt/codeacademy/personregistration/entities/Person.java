ef556180228b927403048eaba15bc6b8c5db81cf
aTomic2022 <tomas.sirvaitis@gmail.com>
2022-10-12 18:12:08 +0300

    Initial commit

---
+++ src/main/java/lt/codeacademy/personregistration/entities/Person.java
@@ -0,0 +1,29 @@
+package lt.codeacademy.personregistration.entities;
+
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+public class Person {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String name;
+
+    @Column
+    private String lastName;
+
+    @Column
+    private String email;
+
+}
---
+++ src/test/java/lt/codeacademy/personregistration/PersonRegistrationApplicationTests.java
@@ -0,0 +1,13 @@
+package lt.codeacademy.personregistration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonRegistrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
